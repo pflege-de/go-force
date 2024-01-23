ad9b44ad270dd5293d499086e6978145071c7247
Jens Hausherr <jens.hausherr@pflege.de>
2024-01-23 17:27:22 +0100

    Convert Limit type to type alias declaration
    Using a type alias enables iterating over limit map

--- force/limits.go
+++ force/limits.go
@@ -1,16 +1,16 @@
 package force
 
-type Limits map[string]Limit
+type Limits = map[string]Limit
 
 type Limit struct {
 	Remaining float64
 	Max       float64
 }
 
-func (forceApi *ForceApi) GetLimits() (limits *Limits, err error) {
+func (forceApi *ForceApi) GetLimits() (limits Limits, err error) {
 	uri := forceApi.apiResources[limitsKey]
 
-	limits = &Limits{}
+	limits = Limits{}
 	err = forceApi.Get(uri, nil, limits)
 
 	return
