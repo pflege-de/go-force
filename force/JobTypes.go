9af126fe08b820c669dd0eebf7ea179d8567dc48
sBurmester <sebastian.burmester@pflege.de>
2024-08-13 16:59:49 +0200

    adds interfaces

--- force/JobTypes.go
+++ force/JobTypes.go
@@ -2,6 +2,8 @@ package force
 
 import (
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type OptionsFunc func(*Job)
@@ -28,4 +30,27 @@ type JobInfo struct {
 	NumberRecordsProcessed int    `json:"numberRecordsProcessed"`
 	JobMessage             string `json:"errorMessage"`
 	ContentURL             string `json:"contentUrl"`
+}
+
+type ForceApiInterface interface {
+	Query(query string, out any) error
+	Get(path string, params url.Values, out any) error
+	Post(path string, params url.Values, payload, out any) error
+	Patch(path string, params url.Values, payload, out any) error
+	GetInstanceURL() string
+	GetAccessToken() string
+	CheckJobStatus(op JobOperation, tickerSeconds time.Duration) (JobOperation, error)
+}
+
+// ForceApiResponse represents a response from salesforce to a fapi.Query() or fapi.Get() request.
+type ForceApiResponse interface {
+	GetDone() bool
+	GetNextRecordsUri() string
+	GetRecords() interface{}
+}
+
+// ForceApiEvent is an interface that any object from SF satisfies. We can use it to query objects other than "EventStream2__c". Kinda legacy
+type ForceApiEvent interface {
+	AllFields() string
+	GetID() string
 }
